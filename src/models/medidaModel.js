e2d73ec4716faef0a2b404021c656db76188a777
jvloliveiraa <joao.oliveira@sptech.school>
2022-05-16 00:01:01 -0300

    Refazendo alterações

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -1,21 +1,17 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
-    instrucaoSql = `select 
-                        temperatura, 
-                        umidade, 
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc limit ${limite_linhas}`;
+function buscarUltimasMedidas(idSensor, limite_linhas) {
+    instrucaoSql = `select top ${limite_linhas} temperaturaAtual, dtHora, FORMAT(dtHora,'%H:%m:%s') 
+                    as momento_grafico from registro
+                    where fkSensor = ${idSensor}
+                    order by idRegistro desc`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 function buscarMedidasEmTempoReal(idSensor) {
-    instrucaoSql = `select top 1 temperaturaAtual from registros where
-    fkSensor = ${idSensor};`;
+    instrucaoSql = `select top 1 temperaturaAtual from registro where
+    fkSensor = ${idSensor} order by idRegistro desc;`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
