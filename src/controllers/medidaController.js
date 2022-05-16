3f3bccc1fe60fcecf5c184e8d8e680cc3b7ff385
jvloliveiraa <joao.oliveira@sptech.school>
2022-05-15 23:46:44 -0300

    Revert "Gráficos quase finalizados"
    This reverts commit 4008223bd92bbf052c100d0f24341292cb44d545.

--- src/controllers/medidaController.js
+++ src/controllers/medidaController.js
@@ -4,11 +4,11 @@ function buscarUltimasMedidas(req, res) {
 
     const limite_linhas = 7;
 
-    var idSensor = req.params.idSensor;
+    var idAquario = req.params.idAquario;
 
     console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
 
-    medidaModel.buscarUltimasMedidas(idSensor, limite_linhas).then(function (resultado) {
+    medidaModel.buscarUltimasMedidas(idAquario, limite_linhas).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -1,17 +1,21 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idSensor, limite_linhas) {
-    instrucaoSql = `select top ${limite_linhas} temperaturaAtual, dtHora, FORMAT(dtHora,'%H:%m:%s') 
-                    as momento_grafico from registro
-                    where fkSensor = ${idSensor}
-                    order by idRegistro desc`;
+function buscarUltimasMedidas(idAquario, limite_linhas) {
+    instrucaoSql = `select 
+                        temperatura, 
+                        umidade, 
+                        momento,
+                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
+                    from medida
+                    where fk_aquario = ${idAquario}
+                    order by id desc limit ${limite_linhas}`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 function buscarMedidasEmTempoReal(idSensor) {
-    instrucaoSql = `select top 1 temperaturaAtual from registro where
-    fkSensor = ${idSensor} order by idRegistro desc;`;
+    instrucaoSql = `select top 1 temperaturaAtual from registros where
+    fkSensor = ${idSensor};`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
